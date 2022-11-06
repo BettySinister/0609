92466807db3f30420c8ef50878945228cb63f6c7
BettySinister <abc_def.3000@mail.ru>
2022-11-06 17:21:13 +0300

    first commit

---
+++ 0609.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+const double e = 2.71828;
+
+double foo(int i) {
+	if (i == 1) {
+		return 1;
+	}
+	else {
+		return foo(i - 1) + pow(e, i) / 100;
+	}
+}
+int main() {
+	cout << "n = ";
+	int input;
+	cin >> input;
+	cout << foo(input);
+}
